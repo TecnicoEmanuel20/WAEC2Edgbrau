62164cfa78f58da5ae8da5f30b5c2fc092bc4548
TecnicoEmanuel20 <perezanchayaedgaremanuel@gmail.com>
2024-05-23 13:57:27 -0500

    iniciando Proyecto

---
+++ src/test/java/pe/edu/cibertec/WAEC2Edgbrau/Waec2EdgbrauApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.WAEC2Edgbrau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Waec2EdgbrauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
